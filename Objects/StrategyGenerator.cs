843380a94c6d4865848aeeedbf988f144ec3a969
Fritz <fritz@junoservices.com>
2026-05-16 09:54:20 +1200

    Initial commit

---
+++ Objects/StrategyGenerator.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Wordprocessing;
+using JCass_ModelCore.Models;
+using JCass_ModelCore.Treatments;
+using JCassDefaultRoadModel.Objects;
+
+namespace JCassDefaultRoadModelV1.Objects;
+
+public class StrategyGenerator
+{
+
+    private ModelBase _frameworkModel;
+    private RoadNetworkModel _domainModel;
+
+    public StrategyGenerator(ModelBase frameworkModel, RoadNetworkModel domainModel)
+    {
+        _frameworkModel = frameworkModel ?? throw new ArgumentNullException(nameof(frameworkModel), "Domain model cannot be null");
+        _domainModel = domainModel ?? throw new ArgumentNullException(nameof(domainModel), "Domain model cannot be null");
+    }
+
+    public List<TreatmentStrategy> GetCandidateStrategies(RoadSegment segment, int period, 
+        Dictionary<string, object> infoFromModel,
+        Dictionary<string, double> numInputs, Dictionary<string, string> textInputs,
+        Dictionary<string, double> numModParamValues, Dictionary<string, string> textModParamValues)
+    {
+        try
+        {
+            List<TreatmentStrategy> strategies = new List<TreatmentStrategy>();
+
+            if (segment.ElementIndex == 522 && period >= 0)
+            {
+                int debug = 0;
+            }
+
+            // Check if the segment passes the Candidate Selection checks. If not, return an empty list.
+            if (segment.IsCandidateForTreatment == 0) return strategies;
+
+            // Although we check if Periods to Next Treatment (i.e. committed) in the Candidate Selection, we need to do it 
+            // again here, because the Candidate Selection result was last evaluated at the last epoch, while the periods to
+            // next treatment have now changed since the period has changed
+            int periodsToNextTreatment = Convert.ToInt32(infoFromModel["periods_to_next_treatment"]);
+            if (periodsToNextTreatment <= 6) { return strategies; }
+
+            int istrat = 0;
+            foreach (var strategySetup in _frameworkModel.StrategiesSetupData)
+            {
+                if( istrat == 41)
+                {
+                    int debug = 0;
+                }
+                try
+                {
+                    if (this.MustTriggerStrategy(strategySetup, segment) == true)
+                    {
+
+                        TreatmentStrategy strategy = new TreatmentStrategy(segment.ElementIndex, strategySetup.StrategyName, numInputs, textInputs, numModParamValues, textModParamValues, period);
+                        strategy.Key = strategySetup.StrategyName;
+
+                        TreatmentInstance firstTreatment = new TreatmentInstance(segment.ElementIndex, strategySetup.FirstTreatment, period,
+                                segment.AreaSquareMetre, strategySetup.ForceFirstTreatment, "no comment", "no reason");
+                        strategy.AddFirstTreatment(firstTreatment);
+
+                        if (strategySetup.Treat2Name != string.Empty)
+                        {
+                            //Note: Wait period will be automatically adjusted by adding 1 to the wait period - because the first treatment is done in the current period
+                            TreatmentInstance followUpTreatment2 = new TreatmentInstance(segment.ElementIndex, strategySetup.Treat2Name, strategySetup.Treat2WaitPeriod,
+                                segment.AreaSquareMetre, strategySetup.Treat2Force, "no comment", "no reason");
+                            strategy.AddFollowUpTreatment(followUpTreatment2, strategySetup.Treat2WaitPeriod);
+                        }
+
+                        if (strategySetup.Treat3Name != string.Empty)
+                        {
+                            //Note: Wait period will be automatically adjusted by adding 1 to the wait period - because the first treatment is done in the current period
+                            TreatmentInstance followUpTreatment3 = new TreatmentInstance(segment.ElementIndex, strategySetup.Treat3Name, strategySetup.Treat3WaitPeriod,
+                                segment.AreaSquareMetre, strategySetup.Treat3Force, "no comment", "no reason");
+                            strategy.AddFollowUpTreatment(followUpTreatment3, strategySetup.Treat3WaitPeriod);
+                        }
+
+                        if (strategySetup.Treat4Name != string.Empty)
+                        {
+                            //Note: Wait period will be automatically adjusted by adding 1 to the wait period - because the first treatment is done in the current period
+                            TreatmentInstance followUpTreatment4 = new TreatmentInstance(segment.ElementIndex, strategySetup.Treat4Name, strategySetup.Treat4WaitPeriod,
+                                segment.AreaSquareMetre, strategySetup.Treat4Force, "no comment", "no reason");
+                            strategy.AddFollowUpTreatment(followUpTreatment4, strategySetup.Treat4WaitPeriod);
+                        }
+
+                        strategies.Add(strategy);
+
+                        // if the first treatment is forced, then do not add any more strategies
+                        if (strategySetup.ForceFirstTreatment == true)
+                        {
+                            break;
+                        }
+                    }
+                    istrat++;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error setting up strategy for '{strategySetup.StrategyName}'; Details: {ex.Message}");
+                }                
+            }
+
+            return strategies;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Error generating candidate strategies for segment {segment.FeebackCode} in period {period}; Details: {ex.Message}");             
+        }        
+    }
+
+    private bool MustTriggerStrategy(StrategySetupInfo strategySetupInfo, RoadSegment segment)
+    {
+        string firstTreatment = strategySetupInfo.FirstTreatment.ToLower();
+
+        // If first treatment is a Second Coat and Surface Function is NOT "1" then do not trigger strategy
+        if (firstTreatment == "chipseal_s" && segment.SurfaceFunction != "1")
+        {
+            return false;
+        }
+
+        if (firstTreatment == "chipseal_h" && segment.SurfaceFunction != "1a")
+        {
+            return false;
+        }
+        else
+        {
+            if (firstTreatment == "chipseal_h" && segment.SurfaceFunction == "1a")
+            {
+                int kk = 0;
+            }            
+        }
+
+        // TODO: Get clarify on surface class names used for Block and Concrete repairs and simplify this logic
+        // If the surface class is CS or AC, we cannot do blockrep, concrep, or xtreat      
+        if (segment.SurfaceClass == "cs" || segment.SurfaceClass == "ac")
+        {
+            List<string> otherTreatments = new List<string>() { "blockrep", "concrep", "xtreat" };
+            if (otherTreatments.Contains(firstTreatment)) return false;
+        }
+
+
+        if (firstTreatment.Contains("seal") && segment.NextSurfaceIsChipSeal == false)
+        {
+            // We need an AC, so cannot do a seal
+            return false;
+        }
+
+        if (firstTreatment.Contains("rehab_cs") && segment.NextSurfaceIsChipSeal == false)
+        {
+            // We need an AC, so cannot do a seal rehab
+            return false;
+        }
+
+        if (firstTreatment.Contains("thinac") && segment.NextSurfaceIsChipSeal == true)
+        {
+            // We need a seal, so cannot do a thin AC
+            return false;
+        }
+
+        if (firstTreatment.Contains("rehab_ac") && segment.NextSurfaceIsChipSeal == true)
+        {
+            // We need a seal, so cannot do a thin AC rehab
+            return false;
+        }
+
+        if (firstTreatment.StartsWith("rehab"))
+        {
+            if (segment.CanRehabFlag == false)
+            {
+                // Cannot rehab this segment
+                return false;
+            }
+
+            string roadClass = segment.RoadType.ToLower();
+            if (segment.NextSurfaceIsChipSeal == true)
+            {
+                string validRehab = "rehab_cs_" + roadClass;
+                if (firstTreatment != validRehab)
+                {
+                    // Invalid rehab treatment for this road class
+                    return false;
+                }
+            }
+
+            if (segment.NextSurfaceIsChipSeal == false)
+            {
+                string validRehab = "rehab_ac_" + roadClass;
+                if (firstTreatment != validRehab)
+                {
+                    // Invalid rehab treatment for this road class
+                    return false;
+                }
+            }
+        }
+
+        // If we get here, all checks passed. So strategy must be triggered
+        return true;
+
+    }
+
+
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+# JCassDefaultRoadModel V1
+
+ Updated Version for the Second Generation Default Road Network Model for Cassandra. 
+ This version is a clone of the BASE model (V0) repository. The BASE C# model 
+ was largely aimed at getting a starting point that matches with the JFunction model.
+
+ Bacause the BASE (V0) model was closely aligned with the JFunction model logic, there 
+ was some complexity in the logic, which was not always necessary for a C# implementation.
+
+ This model (V1) is a clone of the BASE model, but with some improvements to
+ simplify some complex logic and get better consistency in coding convetions.
+
+ ## This Release
+
+ The latests version (commit '1.6.70') needs to be compiled and used with Juno Cassandra Ver 1.6.70 or later. This
+ version has a modified Interface for the Domain Model to facilitate automatic generation of Strategies for the 
+ Benefit-Cost Analysis (BCA) model.
+
+ ## List of Key Changes from JFunction Model
+
+ ### S-Curve Setup
+
+ The setup of S-curve parameters from lookup is now more structured and flexible. It is possible to
+ assign differrent AADI and T100 limits for each distress type. There is a default type, but with small
+ tweaks in the code, separate limits can be assigned for each distress type. Currently, the default type covers
+ all distresses except for Potholes.
+
+ ### S-Curve Resets
+
+ The reset of S-curve parameters previously relied on quite complext logic. Now, the reset logic is split for
+ Rehabs (full reset), Resurfacing (partial reset if distress is above a certain threshold), and Holding Actions
+ (partial reset if distress is above a certain threshold, not the same as for Resurfacing).
+
+ Previous model applied another reset for second-coats over Holding Actions. This is wrong, because the distress
+ will have been already reset by the Holding Action, so when the second-coat is applied, the distress is zero leading
+ to a full reset which may not be warranted if the Preseal is done over a large distress area. New implementation will
+ not reset the S-curve parameters for second-coats over Holding Actions, but will simply continue with the S-curve
+ parameters from the Holding Action (which would have taken into account the distress area when it was reset).
+
+ ### Thin AC Overlays 
+ 
+ With the latest version of the Cassandra Framework model, the cost of a treatment can be split into more than
+ one Budget category. Using this feature, for Thin AC we now use the distress area to automatically calculate the
+ area to be repaired before overlay. This cost is assigned to the 'Preseal Repairs' budget. The overlay cost is then
+ assigned to the "Resurfacing" budget. This can be changed if the clients to not have a 'Preseal Repairs' budget. The
+ treatment cost is then the total cost of the repairs and the overlay.
+
+ The above composite treatment is currently only considred ion the MCDA model, and only if the percentage of Surface 
+ Life Achieved is above a certain percentage (specified in the lookups).
+
+ ### AC Heavy Maintenance Treatment Added
+
+ Based on feedback from the clients, we see a need for a Heavy Maintenance treatment for AC pavements that will fix existing
+ distress but does not include a full overlay. This treatment is considered in the MCDA model if the Achieved Surface Life is 
+ below above a certain percentage (specified in the lookups).
+
+
+ 
